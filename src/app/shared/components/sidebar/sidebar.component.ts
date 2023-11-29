43f96aacdc23361d8ccd53bec80b60298e33fc44
Jason C <grousemusic@gmail.com>
2023-11-29 14:40:13 -0500

    GifService

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private gifsService: GifsService) {
+
+  }
+
+  get tags(){
+    return this.gifsService.tagsHistory;
+  }
 }
